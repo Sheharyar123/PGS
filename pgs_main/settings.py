2b6d8a1bc3f574a37ef67506a2480052389be358
Sheharyar Ahmad <phantomwitch35@gmail.com>
2023-03-15 19:59:35 +0500

    configure settings for static files

--- pgs_main/settings.py
+++ pgs_main/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local
+    "core.apps.CoreConfig",
 ]
 
 MIDDLEWARE = [
@@ -109,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
